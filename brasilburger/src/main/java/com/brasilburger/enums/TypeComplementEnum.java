b2a950890611d1f1b97b5f278ff97bd5a9a6bdf2
Marie Diagne <mariediagne781@gmail.com>
2025-12-13 20:48:04 +0000

    Modele et repository de Complement

--- brasilburger/src/main/java/com/brasilburger/enums/TypeComplementEnum.java
+++ brasilburger/src/main/java/com/brasilburger/enums/TypeComplementEnum.java
@@ -1,6 +1,31 @@
 package com.brasilburger.enums;
 
-public class TypeComplementEnum {
-    
-}
+public enum TypeComplementEnum {
+    BOISSON("BOISSON"),
+    FRITES("FRITES");
 
+    private final String value;
+
+    TypeComplementEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static TypeComplementEnum fromString(String text) {
+        for (TypeComplementEnum type : TypeComplementEnum.values()) {
+            if (type.value.equalsIgnoreCase(text)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException(
+                "Type de complément invalide : " + text + ". Valeurs acceptées : BOISSON, FRITES");
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}

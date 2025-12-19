84e7d5d80f6a2f47e5142c56dc10cfce313eac0e
Marie Diagne <mariediagne781@gmail.com>
2025-12-19 20:28:21 +0000

    Fonctionnalité : Passer une commande"

---
+++ brasilburgercsharp/Controllers/CommandeController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using BrasilBurger.Services.Interfaces;
+using BrasilBurger.Models.Entities;
+using BrasilBurger.Models.Enums;
+using BrasilBurger.Helpers;
+
+namespace BrasilBurger.Controllers
+{
+    public class CommandeController : Controller
+    {
+        private readonly IPanierService _panierService;
+        private readonly ApplicationDbContext _context;
+
+        public CommandeController(IPanierService panierService, ApplicationDbContext context)
+        {
+            _panierService = panierService;
+            _context = context;
+        }
+
+        public IActionResult Passer() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Passer(TypeCommandeEnum type)
+        {
+            var client = HttpContext.Session.Get<Client>("client");
+            var panier = _panierService.GetPanier();
+
+            var commande = new Commande
+            {
+                ClientId = client!.Id,
+                TypeCommande = type,
+                Montant = panier.Total,
+                Etat = EtatCommandeEnum.VALIDE
+            };
+
+            _context.Commandes.Add(commande);
+            await _context.SaveChangesAsync();
+
+            _panierService.Vider();
+            return RedirectToAction("Confirmation");
+        }
+
+        public IActionResult Confirmation() => View();
+    }
+}
